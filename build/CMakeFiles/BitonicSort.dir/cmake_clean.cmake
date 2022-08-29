4128caf393c0ba44117a168f2943f7a80b3df844
LiuZengqiang <12021032@zju.edu.cn>
2022-08-29 16:49:39 +0800

    Modify main.cu Add ./build/

---
+++ build/CMakeFiles/BitonicSort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BitonicSort.dir/main.cu.o"
+  "CMakeFiles/BitonicSort.dir/cmake_device_link.o"
+  "BitonicSort.pdb"
+  "BitonicSort"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/BitonicSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cu
+++ main.cu
@@ -3,23 +3,29 @@
 #include<random>
 #include<ctime>
 #include<iostream>
+#include<cassert>
 #include<cuda_runtime.h>
 #include<driver_types.h>
+#define checkCudaError(val) checkCuda( (val), __FILE__, __LINE__)
+template<typename T>
+void checkCuda(T err, const char* const file, const int line){
+	if(err!=cudaSuccess){
+		std::cerr<<"Error::CUDA:: "<<file<<":"<<line<<std::endl;
+		exit(-1);
+	}
+}
 
 int element_num = 1024;
 int thread_num_per_block = 64;
 
-#define 
 int main(){
 	// 内存
 	int* data_h = nullptr;
 	int* data_d = nullptr;
 	size_t size = element_num * sizeof(int);
 	data_h = (int*)malloc(size);
-	cudaError_t err = cudaSuccess;
-
-	
-
+	checkCudaError(cudaMalloc((void**)&data_d, size));
+	checkCudaError(cudaFree(data_d));
 	// 
 	
 	return 0;
